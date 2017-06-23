c07dafe3f35b97bb10f7ac93dcef5037c70d9248
Semie Rogers <redrogers@gmail.com>
2017-06-23 14:31:16 -0700

    Add clients table

--- server/db/config.js
+++ server/db/config.js
@@ -38,6 +38,19 @@ db.knex.schema.createTableIfNotExists('work_orders', function(orders) {
   orders.foreign('company_id').references('id').inTable('companies');
 });
 
-
+db.knex.schema.createTableIfNotExists('clients', function(clients) {
+  clients.increments('id').primary();
+  clients.string('first_name', 25);
+  clients.string('last_name', 60);
+  clients.string('phone', 15);
+  clients.string('alt_phone', 15);
+  clients.string('street_address_1', 200);
+  clients.string('street_address_2', 200);
+  clients.string('city', 100);
+  clients.string('state_province', 10);
+  clients.string('postal_code', 10);
+  clients.string('country', 15);
+  clients.string('email', 30);
+});
 
 module.exports.db = db;
