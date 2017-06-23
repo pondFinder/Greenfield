d1c349d0fb97e11db519dfbcb1f91e19612484f6
Semie Rogers <redrogers@gmail.com>
2017-06-23 14:20:45 -0700

    Add table work_orders

--- server/db/config.js
+++ server/db/config.js
@@ -20,12 +20,24 @@ db.knex.schema.createTableIfNotExists('users', function(users) {
   users.string('password', 25);
   users.string('first_name', 25);
   users.string('last_name', 60);
-  users.string('role', 30).defaultTo('team_member');
+  users.string('role', 30).defaultTo('user');
   users.dateTime('date');
   users.specifyType('photo', 'BLOB');
   users.string('phone', 15);
   users.string('phone_alt', 15);
   users.foreign('company_id').references('id').inTable('companies');
+});
+
+db.knex.schema.createTableIfNotExists('work_orders', function(orders) {
+  orders.increments('id').primary();
+  orders.text('notes', 'mediumtext');
+  orders.text('job_info', 'mediumtext');
+  orders.dateTime('created_at');
+  orders.boolean('is_done');
+  orders.text('duration', 'mediumtext');
+  orders.foreign('company_id').references('id').inTable('companies');
+});
 
 
-})
+
+module.exports.db = db;
