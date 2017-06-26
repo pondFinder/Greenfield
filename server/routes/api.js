39a5e0ccde616a8b49123fb151cabeb7bb1606d8
Semie Rogers <redrogers@gmail.com>
2017-06-26 13:13:11 -0700

    Rename db to dbHelpers in api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,39 +3,39 @@
  */
 
 var fileToServe = require('./../utility/paths');
-var db = require('./../utility/dbquery');
+var dbHelpers = require('./../utility/dbHelpersquery');
 
 exports.root = (req, res) => {
     res.status(200).send( fileToServe.paths.root );
 };
 
 exports.userSignUp = (req, res) => {
-    var user = db.addUser(req.body);
+    var user = dbHelpers.addUser(req.body);
     res.send(user);
 };
 
 exports.userSignIn = (req, res) => {
-    var user = db.getUser(req.body);
+    var user = dbHelpers.getUser(req.body);
     res.send(user);
 };
 
 exports.createOrder = (req, res) => {
-    var order = db.createOrder(req.body);
+    var order = dbHelpers.createOrder(req.body);
     res.send(order);
 };
 
 exports.updateOrder = (req, res) => {
-    var updatedOrder = db.updateOrder(req.body);
+    var updatedOrder = dbHelpers.updateOrder(req.body);
     res.send(updatedOrder);
 };
 
 exports.completeOrder = (req, res) => {
-    var completedOrder = db.completeOrder(req.body);
+    var completedOrder = dbHelpers.completeOrder(req.body);
     res.send(completedOrder);
 };
 
 exports.deleteOrder = (req, res) => {
-    var deletedOrder = db.deleteOrder(req.body);
+    var deletedOrder = dbHelpers.deleteOrder(req.body);
     res.send(deletedOrder);
 };
 
