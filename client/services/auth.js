a5de4c7254d247824ebd892a6bf9d3f4f0af37f8
Himanshu Pant <himanshu.pn3@gmail.com>
2017-06-30 14:02:39 -0700

    init user sign up and login pages

---
+++ client/services/auth.js
@@ -0,0 +1,13 @@
+angular.module('work-orders').
+  service('auth', function() {
+    this.orderInformation = function(order){
+      this.orderData = order;
+      this.orderStatus
+      if(this.orderData.is_done === false){
+        this.orderStatus = 'In Progress';
+      }else if (this.orderData.is_done === true){
+        this.orderStatus = 'Complete';
+      }
+    }
+
+});
