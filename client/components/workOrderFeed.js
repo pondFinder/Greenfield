ee00d209953512f30dc5e89bdf0bca3ce1336fe7
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-30 17:45:23 -0400

    Changed HTML for create new work order form so it scales nicely on small screens.  Also added logic to submit button so a message is displayed if fields are not entered and the new work order is not created in that case.

--- client/components/workOrderFeed.js
+++ client/components/workOrderFeed.js
@@ -5,7 +5,14 @@ angular.module('work-orders')
   $scope.orders;
   this.currentOrder;
 
+  //Used to show the expanded work order view
   this.show = false;
+
+  //Used to show error text if invalid fields were entered
+  this.invalidFields = false;
+
+  var app = this;
+
   this.expandOrder = function(order){
     dataHandler.setOrderInfo(order);
     this.show = !this.show;
@@ -16,7 +23,7 @@ angular.module('work-orders')
     this.show = !this.show;
   }
   .bind(this);
-  var app = this;
+
 
   this.getWorkOrders = function () {
     // console.log("in get work orders")
@@ -27,14 +34,22 @@ angular.module('work-orders')
       console.log(res.data);
     });
   }.bind(this);
+
+  //Get work orders on page load.
   this.getWorkOrders();
 
-  this.testFunc = function() {
-    console.log("TESTTESTTESTTEST");
-  }
-
   this.createWorkOrder = (e) => {
-    console.log('$ctrl model binds for work-order submit: ', this.woJobDetails, this.woEstimatedDuration, this.woClientName, this.woPhotoUrl);
+    // console.log('$ctrl model binds for work-order submit: ', this.woJobDetails, this.woEstimatedDuration, this.woClientName, this.woPhotoUrl);
+
+    //Alert and return if all fields were not entered
+    if (!this.woJobDetails || !this.woEstimatedDuration || !this.woPhotoUrl || !this.woClientName) {
+      //set invalid fields boolean and immediately return.
+      this.invalidFields = true;
+      return;
+    }
+
+    //reset invalid fields boolean
+    this.invalidFields = false;
 
     //create the current date for created_at
     var currentDate = new Date();
@@ -51,8 +66,12 @@ angular.module('work-orders')
       photo: this.woPhotoUrl,
     };
 
-    console.log(queryObj);
+    this.sendNewOrder(queryObj, this.clearFields);
 
+
+  };
+
+  this.sendNewOrder = function (queryObj, cb) {
     //send HTTP POST to server to create new W.O.
     $http({
       method: 'POST',
@@ -60,11 +79,20 @@ angular.module('work-orders')
       data: queryObj
     })
       .then(function successCallback(res) {
-        console.log(res);
+        cb();
       }, function errorCallback(res) {
         console.log('error creating new order!');
       });
-  };
+  }.bind(this);
+
+  this.clearFields = function () {
+    //Clear all Fields when user clicks submit
+    this.woClientName = '';
+    this.woPhotoUrl = '';
+    this. woJobDetails = '';
+    this.woEstimatedDuration = '';
+
+  }.bind(this);
 
 })
 
