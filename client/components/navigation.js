5725908f9676392188afb0d276058723e881762c
Himanshu Pant <himanshu.pn3@gmail.com>
2017-07-01 03:33:02 -0700

    added logout button

--- client/components/navigation.js
+++ client/components/navigation.js
@@ -1,10 +1,26 @@
 angular.module('work-orders')
 
-.controller('NavCtrl', function() {
+.controller('NavCtrl', function(loginService) {
   this.date = Date().slice(0,16);
+  this.userLogout = function (userData) { // to do: refactor to seperate component
+    // to do: get cur user data/ post to server and destroy session
+    loginService.userLogout(userData, function (res) {
+      if (res === true) {
+        this.parent.isContentHidden = true;
+      } else {
+        console.log('error logging out');
+      }
+    }.bind(this));
+  }
 })
 
 .component('navigation', {
+  require: {
+    parent: '^^app'
+  },
+  bindings: {
+    showLogout: '<'
+  },
   controller: 'NavCtrl',
   templateUrl: '../templates/navigation.html'
 });
